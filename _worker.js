c8c0a067c7d208de2f8e594e18f5c2b51ad18359
EDI SURYANTO <lerengngunung2@gmail.com>
2025-05-18 12:18:47 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
+// 第7行和第9行代码: 生成自己的ID和配置proxyIP
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let proxyIP = '';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
